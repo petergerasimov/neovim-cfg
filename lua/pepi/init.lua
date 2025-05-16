5990bd09e3426d4c9d618f81b1a10fbd5f8a3a8a
Peter Gerasimov <gerasimov@mediahub.eu>
2025-05-16 14:14:27 +0300

    added yank to clipboard
    Signed-off-by: Peter Gerasimov <gerasimov@mediahub.eu>

--- lua/pepi/init.lua
+++ lua/pepi/init.lua
@@ -56,4 +56,4 @@ vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead", "BufWritePost" }, {
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
-
+vim.opt.clipboard:append { 'unnamedplus' }
